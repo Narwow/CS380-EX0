b3f79f42747e95224c7f0e9fe91e5f691c8d7c40
Jhuo Wei Ku <jhuoweiku@cpp.edu>
2017-04-03 22:17:31 -0700

    revision three

--- src/Test2.java
+++
@@ -1,8 +0,0 @@
-
-public class Test2 {
-
-	public void sayHello() {
-		System.out.println("The quick brown fox jumps over the lazy dog.");
-		System.out.println("This is for second revision.");
-	}
-}
