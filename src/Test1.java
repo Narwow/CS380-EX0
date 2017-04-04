2c6359db14520a3b1c41f27d52e532c7e3d5a8aa
Jhuo Wei Ku <jhuoweiku@cpp.edu>
2017-04-03 22:12:26 -0700

    revision two

--- src/Test1.java
+++ src/Test1.java
@@ -3,6 +3,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("The five boxing wizards jump quicklly.");
+		System.out.println("This is for second revision.");
 	}
 
 }
--- src/Test2.java
+++ src/Test2.java
@@ -3,5 +3,6 @@ public class Test2 {
 
 	public void sayHello() {
 		System.out.println("The quick brown fox jumps over the lazy dog.");
+		System.out.println("This is for second revision.");
 	}
 }
